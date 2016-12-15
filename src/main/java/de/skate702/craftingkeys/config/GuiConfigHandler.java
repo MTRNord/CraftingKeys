619fbf4baba086beb9de609d669bd7023613c5ac
Marcel <MTRNord@users.noreply.github.com>
2016-12-15 22:33:37 +0100

    Change GuiID to new API

--- src/main/java/de/skate702/craftingkeys/config/GuiConfigHandler.java
+++ src/main/java/de/skate702/craftingkeys/config/GuiConfigHandler.java
@@ -2,6 +2,7 @@ package de.skate702.craftingkeys.config;
 
 // NEW_1_8 cpw.mods.fml => net.minecraftforge.fml
 
+import de.skate702.craftingkeys.api.Gui;
 import cpw.mods.fml.common.network.IGuiHandler;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
@@ -15,11 +16,13 @@ public class GuiConfigHandler implements IGuiHandler {
 
     @Override
     public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-        if (ID == GuiConfig.GuiID) {
-            return new GuiConfig();
+        if (ID == Gui.GuiID) {
+            return new Gui();
         }
 
         return null;
     }
+
+
 }
 
