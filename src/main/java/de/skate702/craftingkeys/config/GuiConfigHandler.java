04a9b783d9dd90eb1c8e8aab141957575faeef0a
Marcel <MTRNord@users.noreply.github.com>
2016-12-16 18:50:07 +0100

    Change to Singleton

--- src/main/java/de/skate702/craftingkeys/config/GuiConfigHandler.java
+++ src/main/java/de/skate702/craftingkeys/config/GuiConfigHandler.java
@@ -3,7 +3,7 @@ package de.skate702.craftingkeys.config;
 // NEW_1_8 cpw.mods.fml => net.minecraftforge.fml
 
 import de.skate702.craftingkeys.api.Gui;
-import cpw.mods.fml.common.network.IGuiHandler;
+import net.minecraftforge.fml.common.network.IGuiHandler;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
@@ -17,7 +17,7 @@ public class GuiConfigHandler implements IGuiHandler {
     @Override
     public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
         if (ID == Gui.GuiID) {
-            return new Gui();
+            return Gui.getInstance();
         }
 
         return null;
